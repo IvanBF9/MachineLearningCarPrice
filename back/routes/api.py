4df76854ddc96ce939e27b1e20a8d4e6a3a0a3af
IvanBF9 <ivanbraga@outlook.fr>
2023-02-15 16:21:44 +0100

    predict v1

--- back/routes/api.py
+++ back/routes/api.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import numpy
 import re
+import pickle
 
 data = pd.read_csv('../data/dataset.csv')
 
@@ -21,6 +22,24 @@ def create_routes(app):
         numpyArray = numpy.array(carlist)
         df = pd.DataFrame(numpyArray, columns=['cars'])
         df.index += 1
-        res = df['cars'].map(lambda x: x.lstrip().strip('\n'))#re.sub('[^A-Za-z0-9]+', '', x))
+        res = df['cars'].map(lambda x: x.lstrip().strip('\n'))
         return res.to_json()
-        #a = df['column name'].unique()
+
+    @app.route('/api/cars/predict', methods=['POST'])
+    def predict():
+        loaded_model = pickle.load(open('../notebook/model', 'rb'))
+
+        data = {
+            'carmodel' : [34],
+            'miseencirculation': [2016],
+            'kilometrage': [0.133091],
+            'boîtedevitesse': [0.0],
+            'color': [31.0],
+            'car_type': [1.0],
+            'puissancedin': [59]
+        }
+
+        X_predict = pd.DataFrame(data,
+        columns = ['carmodel', 'miseencirculation', 'kilometrage', 'boîtedevitesse', 'color', 'car_type', 'puissancedin'])
+        result = loaded_model.predict(X_predict)
+        return str(result)
